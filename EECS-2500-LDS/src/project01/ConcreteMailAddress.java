ad3684a84730937a44233f800232803721746fb9
Sanskar Lamsal <lamsal9677@yahoo.com>
2020-09-12 14:22:12 -0400

    Trial

---
+++ EECS-2500-LDS/src/project01/ConcreteMailAddress.java
@@ -0,0 +1,51 @@
+package project01;
+
+public class ConcreteMailAddress implements MailAddressInterface {
+
+	public ConcreteMailAddress(String name, String address1, String address2, String city, String state, int zip) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAddressLine1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAddressLine2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getZipCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getZipCodeDigit(int digit) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
